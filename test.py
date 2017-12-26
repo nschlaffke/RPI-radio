ce954180e2078ff96f01ba87467477a67a9fee0c
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2017-12-26 22:43:24 +0100

    [F] Create test.py file to test classes

---
+++ test.py
@@ -0,0 +1,26 @@
+'''
+Dependencies: vlc, keyboard
+keyboard needs root privileges to run properly
+'''
+
+from radio import Radio
+import keyboard
+from time import sleep
+
+stations = ['http://20133.live.streamtheworld.com/SRGSTR02AAC.aac',
+            'http://host4.whooshserver.com:9162/live', 'http://i30.letio.com/9166.aac']
+rd = Radio(stations)
+rd.stationDown()
+while True:
+    try:
+        if keyboard.is_pressed('a'):
+            print('Station up')
+            rd.stationUp()
+            print('Changed to ' + str(rd.getInfo()))
+        if keyboard.is_pressed('b'):
+            print('Station down')
+            rd.stationDown()
+            print('Changed to ' + str(rd.getInfo()))
+        sleep(0.1)
+    except:
+        pass
