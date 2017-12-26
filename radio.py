7bcec894cc21465822f4b835aaa5871b21ec6be3
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2017-12-26 22:42:52 +0100

    [F] Implement Radio class

---
+++ radio.py
@@ -0,0 +1,30 @@
+import vlc
+
+class Radio:
+    def __init__(self, stationsList):
+        self.__instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+        self.__player = self.__instance.media_player_new()
+        self.__stationsList = stationsList
+        self.__numberOfStations = len(stationsList)
+        self.__currentStation = self.__numberOfStations//2
+
+    def play(self):
+        self.__media = self.__instance.media_new(self.__stationsList[self.__currentStation])
+        self.__player.set_media(self.__media)
+        self.__player.play()
+
+    def stationUp(self):
+        if self.__currentStation == self.__numberOfStations - 1:
+            return
+        self.__currentStation += 1
+        self.play()
+
+    def stationDown(self):
+        if self.__currentStation == 0:
+            return
+        self.__currentStation -= 1
+        self.play()
+
+    def getInfo(self):
+        return self.__currentStation
+
